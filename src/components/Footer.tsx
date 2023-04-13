32925e7fe6ed9345b931f4497a20eba3fd1fbdf5
Scstanton20 <98724625+Scstanton20@users.noreply.github.com>
2023-04-13 13:47:10 -0400

    Package updates & Footer auto year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+const date = new Date();
+
 const Footer = () => {
     return (
         <div className="w-full flex flex-col items-start border-t-2 border-slate-800 px-4 py-8 dark:border-opacity-50 mb-20">
             <h1 className="text-black dark:text-white/50 text-2xl font-semibold">Sam Stanton</h1>
-            <h2 className="text-black/60 dark:text-white/30 text-base">Virginia Tech • 2022</h2>
+            <h2 className="text-black/60 dark:text-white/30 text-base">Virginia Tech • {date.getFullYear()}</h2>
             <a className="text-black/60 dark:text-white/30 text-base" href="https://github.com/cnrad/cnrad.dev" target="_blank" rel="noreferrer"> Website design inspired by Conrad Crawford's Personal Portfolio</a>
         </div>
     );

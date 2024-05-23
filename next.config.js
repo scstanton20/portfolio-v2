5f50d8a7cc470b1e5fbba8bdd3bda844cc3fafec
Scstanton20 <98724625+Scstanton20@users.noreply.github.com>
2024-05-22 21:57:41 -0400

    change eslint to not run on build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  // output: 'standalone',
+  eslint: { 
+    ignoreDuringBuilds: true, 
+  }, 
   images: {
     remotePatterns: [
       {
